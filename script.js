6377d9c5b836d9722ed329d52061287ad650fb80
Zane Blood <zaneblood@Zanes-MacBook-Pro.local>
2025-08-25 15:28:44 -0700

    use a gallery approach

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 function showAlert() {
     alert("Repository Downloaded!")
 }
+
+const num_gallery_slides = 5;
+let counter = 1;
+let hideIndex = 1;
+
+function loadNext() {
+
+    counter = counter + 1;
+    if (counter > num_gallery_slides) {
+        counter = counter%num_gallery_slides;
+        hideIndex = num_gallery_slides;
+    }
+    else {
+        hideIndex = counter-1;
+    }
+
+    const slideToHide = document.getElementById("gallery-"+hideIndex);
+    slideToHide.style.display = "none";
+
+    const slideToShow = document.getElementById("gallery-"+counter);
+    slideToShow.style.display = "block";
+}
+
+function loadLast() {
+
+    counter = counter - 1;
+    if (counter < 1) {
+        counter = num_gallery_slides;
+        hideIndex = 1;
+    }
+    else {
+        hideIndex = counter+1;
+    }
+
+    const slideToHide = document.getElementById("gallery-"+hideIndex);
+    slideToHide.style.display = "none";
+
+    const slideToShow = document.getElementById("gallery-"+counter);
+    slideToShow.style.display = "block";
+
+}
